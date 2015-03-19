ec444d3658f5fa66f60613a5f0e37f0b78d467db
yamadapc <tacla.yamada@gmail.com>
2015-03-19 10:34:47 -0300

    Add ELPA headers

--- browse-article.el
+++ browse-article.el
@@ -1,3 +1,22 @@
+;;; browse-article.el --- View articles without webpage cluter, within Emacs
+;; Copyright (C) 2015 Pedro Tacla Yamada
+;;
+;; Author: Pedro Tacla Yamada <tacla.yamada@gmail.com>
+;; Maintainer: Pedro Tacla Yamada <tacla.yamada@gmail.com>
+;; Created: 18 Mar 2015
+;; Keywords: web,browse-url-function,articles
+;; Version: 0.0.1
+;; Homepage: https://github.com/yamadapc/emacs-browse-article
+;; Package-Requires: ((json "1"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary: This package exposes a function `browse-article' to fetch and
+;;; view articles without the clutter within Emacs. It requires `curl' and
+;;; `unfluff' to be installed on the user's machine.
+;;
+;;; Code:
+
 (require 'json)
 
 (defun browse-article--start-process (url buffer)
@@ -33,3 +52,5 @@
          (process (browse-article--start-process url buffer)))
     (set-process-sentinel process (browse-article--make-sentinel buffer))
     (display-buffer buffer)))
+
+;;; browse-article.el ends here
